9b8b72defe343d7b5045a8e58cb38816023a608c
Alexey Minnekhanov <alexeymin@postmarketos.org>
2025-05-01 16:39:09 +0300

    FIXUP iio: smgr: 6.14 compile fix (drop in 6.15)
    should be dropped in 6.15
    
    Signed-off-by: Alexey Minnekhanov <alexeymin@postmarketos.org>

--- drivers/iio/common/qcom_smgr/qcom_smgr.c
+++ drivers/iio/common/qcom_smgr/qcom_smgr.c
@@ -185,10 +185,10 @@ static int qcom_smgr_request_single_sensor_info(struct qcom_smgr *smgr,
 			resp.native_sample_rates[i].rate_count;
 		if (sensor->data_types[i].native_sample_rate_count) {
 			sensor->data_types[i]
-				.native_sample_rates = devm_kmemdup_array(
+				.native_sample_rates = devm_kmemdup(
 				smgr->dev, resp.native_sample_rates[i].rates,
-				sensor->data_types[i].native_sample_rate_count,
-				sizeof(u16), GFP_KERNEL);
+				sensor->data_types[i].native_sample_rate_count * sizeof(u16),
+				GFP_KERNEL);
 			if (!sensor->data_types[i].native_sample_rates)
 				return -ENOMEM;
 		}
