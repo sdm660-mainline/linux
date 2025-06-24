374d0e1dd2ba8d25ce40bfa33c5b9f2e6df0ae23
Alexey Minnekhanov <alexeymin@postmarketos.org>
2025-06-24 19:33:24 +0300

    fixup! drivers/gpu: panel: Add support for Tianma TD4310 panel

--- drivers/gpu/drm/panel/panel-tianma-td4310.c
+++ drivers/gpu/drm/panel/panel-tianma-td4310.c
@@ -7,6 +7,7 @@
 #include <linux/gpio/consumer.h>
 #include <linux/mod_devicetable.h>
 #include <linux/module.h>
+#include <linux/regulator/consumer.h>
 
 #include <video/mipi_display.h>
 
@@ -19,6 +20,13 @@ struct td4310_tianma {
 	struct drm_panel panel;
 	struct mipi_dsi_device *dsi;
 	struct gpio_desc *reset_gpio;
+	struct regulator_bulk_data *supplies;
+};
+
+static const struct regulator_bulk_data td4310_tianma_supplies[] = {
+	{ .supply = "vddio" },
+	{ .supply = "vsn" },
+	{ .supply = "vsp" },
 };
 
 static inline struct td4310_tianma *to_td4310_tianma(struct drm_panel *panel)
@@ -87,12 +95,19 @@ static int td4310_tianma_prepare(struct drm_panel *panel)
 	struct device *dev = &ctx->dsi->dev;
 	int ret;
 
+	ret = regulator_bulk_enable(ARRAY_SIZE(td4310_tianma_supplies), ctx->supplies);
+	if (ret < 0) {
+		dev_err(dev, "Failed to enable regulators: %d\n", ret);
+		return ret;
+	}
+
 	td4310_tianma_reset(ctx);
 
 	ret = td4310_tianma_on(ctx);
 	if (ret < 0) {
 		dev_err(dev, "Failed to initialize panel: %d\n", ret);
 		gpiod_set_value_cansleep(ctx->reset_gpio, 1);
+		regulator_bulk_disable(ARRAY_SIZE(td4310_tianma_supplies), ctx->supplies);
 		return ret;
 	}
 
@@ -110,6 +125,7 @@ static int td4310_tianma_unprepare(struct drm_panel *panel)
 		dev_err(dev, "Failed to un-initialize panel: %d\n", ret);
 
 	gpiod_set_value_cansleep(ctx->reset_gpio, 1);
+	regulator_bulk_disable(ARRAY_SIZE(td4310_tianma_supplies), ctx->supplies);
 
 	return 0;
 }
@@ -150,6 +166,11 @@ static int td4310_tianma_probe(struct mipi_dsi_device *dsi)
 	ctx = devm_kzalloc(dev, sizeof(*ctx), GFP_KERNEL);
 	if (!ctx)
 		return -ENOMEM;
+
+	ret = devm_regulator_bulk_get_const(dev,
+					    ARRAY_SIZE(td4310_tianma_supplies),
+					    td4310_tianma_supplies,
+					    &ctx->supplies);
 
 	ctx->reset_gpio = devm_gpiod_get(dev, "reset", GPIOD_OUT_HIGH);
 	if (IS_ERR(ctx->reset_gpio))
