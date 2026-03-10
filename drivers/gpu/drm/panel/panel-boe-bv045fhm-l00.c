d8dc55d74a07669258f9aa210d6cf8abbdfe2a33
Paul Sajna <sajattack@postmarketos.org>
2026-03-10 00:50:59 -0700

    fixup! drm: panel: Add boe,bv045fhm-l00

--- drivers/gpu/drm/panel/panel-boe-bv045fhm-l00.c
+++ drivers/gpu/drm/panel/panel-boe-bv045fhm-l00.c
@@ -7,6 +7,7 @@
 #include <linux/gpio/consumer.h>
 #include <linux/mod_devicetable.h>
 #include <linux/module.h>
+#include <linux/regulator/consumer.h>
 
 #include <drm/drm_mipi_dsi.h>
 #include <drm/drm_modes.h>
@@ -17,6 +18,13 @@ struct boe_bv045fhm_l00 {
 	struct drm_panel panel;
 	struct mipi_dsi_device *dsi;
 	struct gpio_desc *reset_gpio;
+	struct regulator_bulk_data* supplies;
+};
+
+static const struct regulator_bulk_data boe_bv045fhm_l00_supplies[] = {
+	{ .supply = "vddio"},
+	{ .supply = "vddneg"},
+	{ .supply = "vddpos"},
 };
 
 static inline struct boe_bv045fhm_l00 *to_boe_bv045fhm_l00(struct drm_panel *panel)
@@ -125,6 +133,10 @@ static int boe_bv045fhm_l00_prepare(struct drm_panel *panel)
 	struct device *dev = &ctx->dsi->dev;
 	int ret;
 
+	ret = regulator_bulk_enable(ARRAY_SIZE(boe_bv045fhm_l00_supplies), ctx->supplies);
+	if (ret < 0)
+		return ret;
+
 	boe_bv045fhm_l00_reset(ctx);
 
 	ret = boe_bv045fhm_l00_on(ctx);
@@ -148,6 +160,8 @@ static int boe_bv045fhm_l00_unprepare(struct drm_panel *panel)
 		dev_err(dev, "Failed to un-initialize panel: %d\n", ret);
 
 	gpiod_set_value_cansleep(ctx->reset_gpio, 1);
+
+	regulator_bulk_disable(ARRAY_SIZE(boe_bv045fhm_l00_supplies), ctx->supplies);
 
 	return 0;
 }
@@ -190,6 +204,16 @@ static int boe_bv045fhm_l00_probe(struct mipi_dsi_device *dsi)
 				   DRM_MODE_CONNECTOR_DSI);
 	if (IS_ERR(ctx))
 		return PTR_ERR(ctx);
+
+
+	ret = devm_regulator_bulk_get_const(dev, ARRAY_SIZE(boe_bv045fhm_l00_supplies),
+		boe_bv045fhm_l00_supplies,
+		&ctx->supplies
+	);
+
+	if (ret < 0)
+		return dev_err_probe(dev, ret, "Failed to get regulators\n");
+
 
 	ctx->reset_gpio = devm_gpiod_get(dev, "reset", GPIOD_OUT_HIGH);
 	if (IS_ERR(ctx->reset_gpio))
